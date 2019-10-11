9bdd75a099d1c29171dc1c3e98c5e2ae72c28d0b
ash1995 <ash1995@gmail.com>
2019-10-11 12:14:42 +0400

    Add xview2 dataset

---
+++ research/deeplab/datasets/convert_xview2.sh
@@ -0,0 +1,2 @@
+rm -r /home/an1/deeplabv3plus/data/xView2/tfrecord/*
+python build_xview2_data.py
--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -76,6 +76,14 @@ _CITYSCAPES_INFORMATION = DatasetDescriptor(
     ignore_label=255,
 )
 
+_XVIEW2_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 5598,
+    },
+    num_classes=2,
+    ignore_label=255,
+)
+
 _PASCAL_VOC_SEG_INFORMATION = DatasetDescriptor(
     splits_to_sizes={
         'train': 1464,
@@ -100,6 +108,7 @@ _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'xView2': _XVIEW2_INFORMATION
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
